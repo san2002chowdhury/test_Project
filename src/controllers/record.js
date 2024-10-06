4bc6f4b4d3a1a25aac70a92e3ec28366a840c6d0
san2002chowdhury <sunilkrchowdhury66@gmail.com>
2024-10-06 21:05:23 +0530

    second

--- src/controllers/record.js
+++ src/controllers/record.js
@@ -6,6 +6,7 @@ const { body, check, validationResult } = require("express-validator");
 const { Record } = require("../models");
 
 const RecordController = Router();
+//Insert data in the dataBase
 RecordController.post(
   "/add",
   check("name").not().isEmpty().withMessage("question is required"),
@@ -24,6 +25,7 @@ RecordController.post(
       });
     } else {
       try {
+        const allData = await Record.find({});
         const data = req.body;
         const nameFound = await Record.findOne({ name: data.name });
         if (nameFound !== null) {
@@ -32,6 +34,7 @@ RecordController.post(
           });
         } else {
           console.log("We are in Else");
+          data.sl_No = data.sl_No + allData.length;
           let recodData = new Record(data);
           recodData.save();
           res.status(201).send({
@@ -45,4 +48,72 @@ RecordController.post(
     }
   }
 );
+//Get All data present in the database
+RecordController.get("/data", async (req, res, next) => {
+  try {
+    const allData = await Record.find({});
+    res.status(200).json({
+      result: `We get total ${allData.length} data`,
+      data: [allData],
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+//Update in the data base
+RecordController.patch("/update/:name", async (req, res, next) => {
+  try {
+    const name = req.params.name;
+    const data = await Record.findOne({
+      name: { $regex: name, $options: "im" },
+    });
+    console.log(data);
+    if (data) {
+      const _id = data._id;
+
+      const dataUpdate = req.body;
+      console.log(dataUpdate);
+
+      const updateData = await Record.findByIdAndUpdate(_id, dataUpdate);
+      res.status(200).json({
+        result: "update done successfully",
+        updateData,
+      });
+    } else {
+      res.status(404).json({
+        result: "Invalid Data",
+      });
+    }
+  } catch (error) {
+    res.status(404).json({
+      result: "wrong patching",
+      error: error,
+    });
+  }
+});
+RecordController.delete("/delete/:name", async (req, res, next) => {
+  try {
+    const name = req.params.name;
+    const data = await Record.findOne({
+      name: { $regex: name, $options: "im" },
+    });
+    if (data) {
+      const _id = data._id;
+      const deletedData = await Record.findByIdAndDelete(_id);
+      res.status(200).json({
+        result: "delete done successfully",
+        deletedData,
+      });
+    } else {
+      res.status(404).json({
+        result: "Invalid Data",
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      result: "wrong deleting",
+      error: error,
+    });
+  }
+});
 module.exports = RecordController;
